8e0c83e619e20324e4f2d802f0efce5b15cbc63c
reservanaturalsalus <reservanaturalsalus@gmail.com>
2026-04-09 01:35:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById("salusForm").addEventListener("submit", async function(e
   const form = e.target;
   const data = new FormData(form);
 
-  const endpoint = "URL_DE_TU_WEB_APP_AQUÍ"; // <- la cambiás cuando actives el script
+  const endpoint = "https://script.google.com/macros/s/AKfycbzY5j7Jxxz_LOwPWr7gjEFqdY-4o11jha3Bb30UAUt4Yo6qHxe1Q_XMLXjpTzCnqav4YQ/exec"; // <- la cambiás cuando actives el script
 
   const response = await fetch(endpoint, {
     method: "POST",
